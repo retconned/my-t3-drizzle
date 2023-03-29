25aca9b989a260382794e5d99e16d2b66636b369
Henrik Wenz <henrik.wenz@gmail.com>
2023-03-30 00:40:03 +0200

    Prevent pooling issues due to hmr

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,18 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import mysql, { type Pool } from "mysql2/promise";
 
-const poolConnection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const globalForMySQL = globalThis as unknown as { poolConnection: Pool };
+
+const poolConnection =
+  globalForMySQL.poolConnection ||
+  mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+if (process.env.NODE_ENV !== "production")
+  globalForMySQL.poolConnection = poolConnection;
 
 export const db = drizzle(poolConnection);
